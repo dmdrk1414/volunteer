833a1fdcd5ceb4c67a381546fa408a6d121cd228
dmdrk1414 <seungchan141414@gmail.com>
2023-08-13 22:53:29 +0900

    components Manu 추가

---
+++ front/src/view/components/Manu.js
@@ -0,0 +1,50 @@
+/* eslint-disable-next-line  */
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  Button,
+  FormControl,
+  Container,
+} from "react-bootstrap";
+/* eslint-disable-next-line  */
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+function Manu() {
+  return (
+    // 무조건 div는 하나
+    <div className="manu">
+      <Navbar bg="light" expand="lg" className="navbar-container">
+        <Container>
+          <Navbar.Brand href="#home" className="navber_title">
+            <Link to="/">image </Link>
+            <Link to="/">광주 봉사 포털</Link>
+          </Navbar.Brand>
+          <Navbar>
+            <Nav className="justify-content-arount">
+              <Nav.Link className="nav_items">
+                <Link to="#action/3.1">봉사 신청</Link>
+              </Nav.Link>
+              <Nav.Link className="nav_items">
+                <Link to="#action/3.2">신청 현황</Link>
+              </Nav.Link>
+              <NavDropdown title="나의 자원 봉사">
+                <NavDropdown.Item href="/#action/3.3">로그인</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">
+                  나의 신청현황
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+/* eslint-disable-next-line  */
+export default Manu;
--- front/src/view/router/Home.js
+++ front/src/view/router/Home.js
@@ -3,9 +3,14 @@ import React from "react";
 import axios from "axios";
 
 // component
+import Manu from "../components/Manu";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Manu></Manu>
+    </div>
+  );
 }
 
 export default Home;
