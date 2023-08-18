d948208c5f05c5beae339005639b0ca520213630
jeun0517 <dbswjddms517@naver.com>
2023-08-19 00:20:55 +0900

    [Feat] 봉사활동 게시판 domain

---
+++ volunteer/src/main/java/backend/springbootdeveloper/jungeun/domain/VolunteerList.java
@@ -0,0 +1,49 @@
+package backend.springbootdeveloper.jungeun.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class VolunteerList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "region", nullable = false)
+    private String region;
+
+    @Column(name = "adOffice", nullable = false)
+    private String adOffice;
+
+    @Column(name = "adPeriod", nullable = false)
+    private String adPeriod;
+
+    @Column(name = "volunteerPeriod", nullable = false)
+    private String volunteerPeriod;
+
+    @Column(name = "remainPeriod", nullable = false)
+    private String remainPeriod;
+
+    @Builder
+    public VolunteerList(long id, String title, String region, String adOffice, String adPeriod, String volunteerPeriod, String remainPeriod){
+        this.id = id;
+        this.title = title;
+        this.region = region;
+        this.adOffice = adOffice;
+        this.adPeriod = adPeriod;
+        this.volunteerPeriod = volunteerPeriod;
+        this.remainPeriod = remainPeriod;
+    }
+}
