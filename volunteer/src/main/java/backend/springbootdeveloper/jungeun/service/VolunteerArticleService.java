a22ab55a3b560e678e052f5668cee720b12241a9
jeun0517 <dbswjddms517@naver.com>
2023-08-19 02:04:14 +0900

    [Feat] findById 추가

--- volunteer/src/main/java/backend/springbootdeveloper/jungeun/service/VolunteerArticleService.java
+++ volunteer/src/main/java/backend/springbootdeveloper/jungeun/service/VolunteerArticleService.java
@@ -1,6 +1,7 @@
 package backend.springbootdeveloper.jungeun.service;
 
 import backend.springbootdeveloper.jungeun.domain.VolunteerArticle;
+import backend.springbootdeveloper.jungeun.domain.VolunteerList;
 import backend.springbootdeveloper.jungeun.repository.VolunteerArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,12 @@ import java.util.List;
 public class VolunteerArticleService {
     private final VolunteerArticleRepository volunteerArticleRepository;
 
-    public List<VolunteerArticle> findAll(){
+    public List<VolunteerList> findAll(){
         return volunteerArticleRepository.findAll();
     }
+    // 이게 게시물인지 게시판인지 생각, repository에 선언한거 때문에 오류난듯
 
+    public VolunteerArticle findById(long id){
+        List<VolunteerArticle> list =
+    }
 }
