ff9efaa5a42af8d8a3545a38c3fca003c0e35dd1
jeun0517 <dbswjddms517@naver.com>
2023-08-19 02:03:12 +0900

    [Feat] 봉사활동 controller

--- volunteer/src/main/java/backend/springbootdeveloper/jungeun/controller/VolunteerArticleController.java
+++ volunteer/src/main/java/backend/springbootdeveloper/jungeun/controller/VolunteerArticleController.java
@@ -1,11 +1,15 @@
 package backend.springbootdeveloper.jungeun.controller;
 
+import backend.springbootdeveloper.jungeun.domain.VolunteerArticle;
+import backend.springbootdeveloper.jungeun.dto.response.EachVolunteerArticleResponse;
 import backend.springbootdeveloper.jungeun.dto.response.VolunteerArticleResponse;
 import backend.springbootdeveloper.jungeun.service.VolunteerArticleService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,13 +20,19 @@ public class VolunteerArticleController {
     private final VolunteerArticleService volunteerArticleService;
 
     @GetMapping("/article/volunteer")
-    public ResponseEntity<List<VolunteerArticleResponse>> findAllVolunteerArticle(){
-        List<VolunteerArticleResponse> volunteerArticle = volunteerArticleService.findAll()
+    public ResponseEntity<List<VolunteerArticleResponse>> findAllVolunteerList(){
+        List<VolunteerArticleResponse> volunteerList = volunteerArticleService.findAll()
                 .stream()
                 .map(VolunteerArticleResponse::new)
                 .toList();
-
-        return ResponseEntity.ok().body(volunteerArticle);
+        return ResponseEntity.ok().body(volunteerList);
     }
 
+    @GetMapping("/article/volunteer/{id}")
+    public ResponseEntity<EachVolunteerArticleResponse> findAllVolunteerArticleById(@PathVariable long id){
+        VolunteerArticle volunteerArticle = volunteerArticleService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new EachVolunteerArticleResponse(volunteerArticle));
+    }
 }
