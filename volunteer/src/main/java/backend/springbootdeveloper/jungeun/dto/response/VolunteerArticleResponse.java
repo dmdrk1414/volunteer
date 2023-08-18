a396075f39becff93b35c1230c711ad0198cda91
jeun0517 <dbswjddms517@naver.com>
2023-08-18 23:03:54 +0900

    [Feat] 봉사활동 게시믈 response dto

---
+++ volunteer/src/main/java/backend/springbootdeveloper/jungeun/dto/response/VolunteerArticleResponse.java
@@ -0,0 +1,35 @@
+package backend.springbootdeveloper.jungeun.dto.response;
+
+import backend.springbootdeveloper.jungeun.domain.VolunteerArticle;
+import lombok.Getter;
+
+@Getter
+public class VolunteerArticleResponse {
+    private Long id;
+    private String title;
+    private String volunteerPeriod;
+    private String volunteerTime;
+    private String adPeriod;
+    private String volunteerPlace;
+    private int adPeople;
+    private int applicationPeople;
+    private String adOffice;
+    private String volunteerType;
+    private String content;
+    private String recruitment;
+
+    public VolunteerArticleResponse(VolunteerArticle volunteerArticle){
+        this.id = volunteerArticle.getId();
+        this.title = volunteerArticle.getTitle();
+        this.volunteerPeriod = volunteerArticle.getVolunteerPeriod();
+        this.volunteerTime = volunteerArticle.getVolunteerTime();
+        this.adPeriod = volunteerArticle.getAdPeriod();
+        this.volunteerPlace = volunteerArticle.getVolunteerPlace();
+        this.adPeople = volunteerArticle.getAdPeople();
+        this.applicationPeople = volunteerArticle.getApplicationPeople();
+        this.adOffice = volunteerArticle.getAdOffice();
+        this.volunteerType = volunteerArticle.getVolunteerType();
+        this.content = volunteerArticle.getContent();
+        this.recruitment = volunteerArticle.getRecruitment();
+    }
+}
