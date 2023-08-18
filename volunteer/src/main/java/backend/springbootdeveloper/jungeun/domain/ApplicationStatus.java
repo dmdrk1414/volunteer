421de08b945572bc1e2cbc0aad45d0fe989871e8
jeun0517 <dbswjddms517@naver.com>
2023-08-18 22:55:56 +0900

    [Feat] domain 기능 추가

--- volunteer/src/main/java/backend/springbootdeveloper/jungeun/domain/ApplicationStatus.java
+++ volunteer/src/main/java/backend/springbootdeveloper/jungeun/domain/ApplicationStatus.java
@@ -1,5 +1,9 @@
 package backend.springbootdeveloper.jungeun.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,17 +13,42 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ApplicationStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
     private long id; // 게시물 고유 아이디
+
+    @Column(name = "title", updatable = false)
     private String title; // 게시물 제목
+
+    @Column(name = "volunteerPeriod", updatable = false)
     private String volunteerPeriod; // 봉사 기간
+
+    @Column(name = "volunteerTime", updatable = false)
     private String volunteerTime; // 봉사 시간
+
+    @Column(name = "adPeriod", updatable = false)
     private String adPeriod; // 모집 기간
+
+    @Column(name = "volunteerPlace", updatable = false)
     private String volunteerPlace; // 봉사 장소
+
+    @Column(name = "adPeople", updatable = false)
     private int adPeople; // 모집 인원
+
+    @Column(name = "applicationPeople", updatable = false)
     private int applicationPeople; // 신청 인원
+
+    @Column(name = "adOffice", updatable = false)
     private String adOffice; // 모집 기관
+
+    @Column(name = "volunteerType", updatable = false)
     private String volunteerType; // 봉사자 유형
+
+    @Column(name = "content", updatable = false)
     private String content; // 게시물 내용
+
+    @Column(name = "recruitment", updatable = false)
     private String recruitment; // 모집 여부
 
     @Builder
