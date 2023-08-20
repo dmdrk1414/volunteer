126664387a594ba27fcf33c0b4415d73fbfcef74
jeun0517 <dbswjddms517@naver.com>
2023-08-21 01:01:11 +0900

    [Feat] get 메서드 추가

---
+++ volunteer/src/main/java/backend/springbootdeveloper/jungeun/service/ArticleService.java
@@ -0,0 +1,19 @@
+package backend.springbootdeveloper.jungeun.service;
+
+import backend.springbootdeveloper.jungeun.domain.VolunteerArticle;
+import backend.springbootdeveloper.jungeun.repository.JpaArticleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArticleService {
+    private final JpaArticleRepository articleRepository;
+    public ArticleService(){
+        this.articleRepository = new JpaArticleRepository();
+    }
+
+    public List<VolunteerArticle> getArticleOfVolunteer(){
+        return articleRepository.findAllOfVolunteerTable();
+    }
+}
