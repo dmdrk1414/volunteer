14cf864baad4e443cc54da611fc3d630d370a54d
jeun0517 <dbswjddms517@naver.com>
2023-08-21 01:01:30 +0900

    [Fix] import 선언 수정

---
+++ volunteer/src/main/java/backend/springbootdeveloper/jungeun/repository/JpaArticleRepository.java
@@ -0,0 +1,20 @@
+package backend.springbootdeveloper.jungeun.repository;
+
+import backend.springbootdeveloper.jungeun.Util.TempDB;
+import backend.springbootdeveloper.jungeun.domain.VolunteerArticle;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class JpaArticleRepository {
+    private TempDB DB;
+
+    public JpaArticleRepository(){
+        this.DB = new TempDB();
+    }
+
+    public List<VolunteerArticle> findAllOfVolunteerTable(){
+        return DB.getVolunteer();
+    }
+}
