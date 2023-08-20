a4aad2218a6d6f1c22b2021cd44d73e6c0b9122a
jeun0517 <dbswjddms517@naver.com>
2023-08-21 01:16:33 +0900

    [Comment] 파일 전체 주석 처리

--- volunteer/src/main/java/backend/springbootdeveloper/jungeun/repository/VolunteerArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.springbootdeveloper.jungeun.repository;
-
-import backend.springbootdeveloper.jungeun.domain.VolunteerArticle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VolunteerArticleRepository extends JpaRepository<VolunteerArticle, Long> {
-}
