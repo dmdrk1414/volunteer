f25c6354e309ab289f6b0f3da7fb14d8b1dc51e1
jeun0517 <dbswjddms517@naver.com>
2023-08-21 00:48:33 +0900

    [Feat] service 추가 수정

--- volunteer/src/main/java/backend/springbootdeveloper/jungeun/service/VolunteerArticleService.java
+++ volunteer/src/main/java/backend/springbootdeveloper/jungeun/service/VolunteerArticleService.java
@@ -2,7 +2,8 @@ package backend.springbootdeveloper.jungeun.service;
 
 import backend.springbootdeveloper.jungeun.domain.VolunteerArticle;
 import backend.springbootdeveloper.jungeun.domain.VolunteerList;
-import backend.springbootdeveloper.jungeun.repository.VolunteerArticleRepository;
+import backend.springbootdeveloper.jungeun.repository.ArticleRepository;
+import backend.springbootdeveloper.jungeun.repository.VolunteerListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +12,19 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class VolunteerArticleService {
-    private final VolunteerArticleRepository volunteerArticleRepository;
-
+    private final ArticleService articleService;
     public List<VolunteerList> findAll(){
-        return volunteerArticleRepository.findAll();
+        return articleService.getArticleOfVolunteer();
     }
-    // 이게 게시물인지 게시판인지 생각, repository에 선언한거 때문에 오류난듯
 
-    public VolunteerArticle findById(long id){
-        List<VolunteerArticle> list =
+
+    public VolunteerList findById(long id){
+        List<VolunteerList> list = articleService.getArticleOfVolunteer();
+        for(VolunteerList article : list){
+            if(id == article.getId()){
+                return article;
+            }
+        }
+        throw new IllegalArgumentException("각각의 게시물 예외 처리");
     }
 }
