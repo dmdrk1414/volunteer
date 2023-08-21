6951075fd92a1f360f28313834d78601cc8665d1
Yun-tellije <dbswjddms517@gmail.com>
2023-08-21 17:54:01 +0900

    [Test] test 코드 작성

--- volunteer/src/main/java/backend/springbootdeveloper/jungeun/controller/TestController.java
+++ volunteer/src/main/java/backend/springbootdeveloper/jungeun/controller/TestController.java
@@ -1,13 +1,33 @@
 package backend.springbootdeveloper.jungeun.controller;
 
+import backend.springbootdeveloper.jungeun.domain.VolunteerArticle;
 import backend.springbootdeveloper.jungeun.repository.VolunteerListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     @Autowired
     private VolunteerListRepository volunteerListRepository;
+
+    @GetMapping("/test")
+    public String testController(){
+        volunteerListRepository.save(VolunteerArticle.builder()
+                .title("봉사모집")
+                .volunteerPeriod("내일까지")
+                .volunteerTime("4시간")
+                .adPeriod("내일까지")
+                .volunteerPlace("우리집")
+                .adPeople(2)
+                .applicationPeople(10)
+                .adOffice("우리집")
+                .volunteerType("착한 사람")
+                .content("봉사할 사람 구함")
+                .recruitment("모집 중")
+                .build());
+        return "Hello world";
+    }
 
 
 }
