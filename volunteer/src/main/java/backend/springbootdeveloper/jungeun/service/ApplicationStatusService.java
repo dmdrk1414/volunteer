10f6d0194e54d149830f0b75215bda472760ac9a
jeun0517 <dbswjddms517@naver.com>
2023-09-08 23:33:04 +0900

    [Feat] 기능 수정

--- volunteer/src/main/java/backend/springbootdeveloper/jungeun/service/ApplicationStatusService.java
+++ volunteer/src/main/java/backend/springbootdeveloper/jungeun/service/ApplicationStatusService.java
@@ -1,6 +1,7 @@
 package backend.springbootdeveloper.jungeun.service;
 
 import backend.springbootdeveloper.jungeun.domain.ApplicationStatus;
+import backend.springbootdeveloper.jungeun.domain.VolunteerArticle;
 import backend.springbootdeveloper.jungeun.repository.ApplicationStatusListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,15 @@ public class ApplicationStatusService {
 
     public List<ApplicationStatus> findAll(){
         return applicationStatusListRepository.findAll();
+    }
+
+    public ApplicationStatus findById(long id){
+//        List<VolunteerArticle> list = articleService.getArticleOfVolunteer();
+//        for(VolunteerArticle article : list){
+//            if(id == article.getId()){
+//                return article;
+//            }
+//        }
+        throw new IllegalArgumentException("각각의 게시물 예외 처리");
     }
 }
